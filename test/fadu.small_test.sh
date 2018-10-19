68e301cca4ec592c9d2c80579a2e15006b4fba2f
Shaun Adkins <sadkins@som.umaryland.edu>
2018-10-19 08:49:42 -0400

    removed files related to v2 python version of FADU

--- test/fadu.small_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-gff3="./GCF_000008385.1_ASM838v1_genomic.gff"
-tempdir="./FADU_tmp"
-outputdir="./FADU"
-bam="./test.bam"
-
-python3 ../fadu.v2.py --bam_file $bam --gff3 $gff3 --tmp_dir $tempdir --output_dir $outputdir --stranded yes --feature_type CDS --attribute_type ID --count_by fragment --keep_only_properly_paired --debug="DEBUG"
