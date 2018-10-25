9aaac491e02f0e034cc4082bdb420117984a22fd
Shaun Adkins <sadkins@som.umaryland.edu>
2018-10-25 15:14:06 -0400

    fixing strand conversion issue

--- fadu.jl
+++ fadu.jl
@@ -92,7 +92,8 @@ function compute_frag_feat_ratio(uniq_coords::Dict{String, Dict}, fragment::Inte
         # Pertinent fragment info
         strand = '+'
         if stranded
-            strand = convert(Char, strand(fragment))
+            # for some reason, strand(fragment) throws an error
+            strand = convert(Char, fragment.strand)
         end
         frag_start = leftposition(fragment)
         frag_end = rightposition(fragment)
